f566b602bb4b473b42bc28fda1bd20141689ace5
Guilhermedelaporta <guilhermedelaporta1@gmail.com>
2024-12-09 00:07:47 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function verMais() {
+    let htmlFundamentos = document.getElementById("htmlFundamentos")
+    let cssFundamentos = document.getElementById("cssFundamentos");
+    let javaScriptFundamentos = document.getElementById("javaScriptFundamentos")
+    let domFundamentos = document.getElementById("domFundamentos")
+    let acessJs = document.getElementById("acessJs")
+    let estSeo = document.getElementById("estSeo")
+    let desRes = document.getElementById("desRes")
+
+    let abrirMenu = document.createElement("aside")
+
+    
+}
